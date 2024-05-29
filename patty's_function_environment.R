bb65e1d01c9b6f9ffea4d98c07712ef949404c96
Patrick Monari <61850735+patrickmonari@users.noreply.github.com>
2024-05-29 15:24:02 +1200

    Add files via upload

---
+++ patty's_function_environment.R
@@ -0,0 +1,19 @@
+#### Some of John Curtin's functions ####
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_modeling.R?raw=true")
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_ml.R?raw=true")
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_eda.R?raw=true")
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_plots.R?raw=true")
+
+##### My functions ####
+
+# SEM dataframe maker
+devtools::source_url("https://github.com/patrickmonari/rsupport/blob/main/summarySE.R?raw=true")
+
+# Model table maker
+devtools::source_url("https://github.com/patrickmonari/rsupport/blob/main/generate_lm_table.R?raw=true")
+devtools::source_url("https://github.com/patrickmonari/rsupport/blob/main/generate_lmer_table.R?raw=true")
+devtools::source_url("https://github.com/patrickmonari/rsupport/blob/main/combine_tables_into_grid.R?raw=true")
+
+# For bootstrapped coefficient estimates
+devtools::source_url("https://github.com/patrickmonari/rsupport/blob/main/lm_bootstrapping.R?raw=true")
+devtools::source_url("https://github.com/patrickmonari/rsupport/blob/main/lmer_bootstrapping.R?raw=true")
